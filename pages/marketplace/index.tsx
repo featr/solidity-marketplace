7f7944c19efee354b081b9b6d13bb93fcef165b3
Evlogi Aleksandrov <evlogialeksandrov@Evlogis-MacBook-Air.local>
2022-06-09 18:22:28 +0300

    added purchaseCourse interaction with the smart contract

--- pages/marketplace/index.tsx
+++ pages/marketplace/index.tsx
@@ -1,4 +1,5 @@
 import { useWalletInfo } from "@components/hooks/web3";
+import { useWeb3 } from "@components/providers";
 import { ConnectButton } from "@components/ui/common";
 import { CourseCard, CourseList } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
@@ -8,13 +9,42 @@ import { CourseContent, getAllCourses } from "@content/courses/fetcher";
 import { useState } from "react";
 
 export default function Marketplace({ courses }: { courses: CourseContent[] }) {
+  const { web3, contract } = useWeb3();
+  const { canPurchaseCourse, account } = useWalletInfo();
   const [selectedCourse, setSelectedCourse] = useState<null | CourseContent>(
     null
   );
-  const { canPurchaseCourse } = useWalletInfo();
 
-  const purchaseCourse = (order) => {
-    alert(JSON.stringify(order));
+  const purchaseCourse = async (order) => {
+    const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id);
+
+    const courseHash = web3.utils.soliditySha3(
+      { type: "bytes16", value: hexCourseId },
+      { type: "address", value: account.data }
+    );
+
+    const emailHash = web3.utils.sha3(order.email);
+
+    const proof = web3.utils.soliditySha3(
+      {
+        type: "bytes32",
+        value: emailHash,
+      },
+      {
+        type: "bytes32",
+        value: courseHash,
+      }
+    );
+
+    const coursePrice = web3.utils.toWei(order.price.toString());
+
+    try {
+      await contract.methods
+        .purchaseCourse(hexCourseId, proof)
+        .send({ from: account.data, value: coursePrice });
+    } catch {
+      console.log("Purchase course: Operation has failed.");
+    }
   };
 
   return (
