5a8992cb56d4a236ca987e2f2c83322339b004b7
Evlogi Aleksandrov <evlogialeksandrov@Evlogis-MacBook-Air.local>
2022-06-11 09:12:40 +0300

    WalletBar style improvements

--- components/ui/common/button/Button.tsx
+++ components/ui/common/button/Button.tsx
@@ -18,11 +18,12 @@ const ConnectButton = ({
     purple: `text-white bg-indigo-600 ${hoverable && "hover:bg-indigo-700"}`,
     red: "text-white bg-red-600 hover:bg-red-700",
     lightPurple: `text-indigo-700 bg-indigo-100 ${hoverable && "hover:bg-indigo-200"}`,
+    white: `text-black bg-white`
   };
   return (
     <button
       {...rest}
-      className={`px-8 py-3 border rounded-md text-base font-medium disabled:opacity-50 disabled:cursor-not-allowed ${className} ${variants[variant]}`}
+      className={`xs:px-8 xs:py-3 border rounded-md text-base font-medium disabled:opacity-50 disabled:cursor-not-allowed ${className} ${variants[variant]}`}
     >
       {children}
     </button>
